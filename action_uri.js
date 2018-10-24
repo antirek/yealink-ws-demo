fa4f4b7df2ba844ceaa8989cba1cdd2ad8f3dfd8
Sergey Dmitriev <serge.dmitriev@gmail.com>
2018-10-24 23:36:43 +0700

    add action uri list

---
+++ action_uri.js
@@ -0,0 +1,186 @@
+module.exports = [
+    {
+        value: "OK",
+        desc: "Press the OK/√ key."
+    },
+    {
+        value: "ENTER",
+        desc: "Press the Enter soft key."
+    },
+    {
+        value: "SPEAKER",
+        desc: "Press the Speakerphone key."
+    },
+    {
+        value: "F_TRANSFER",
+        desc: "Transfers a call to another party."
+    },
+    {
+        value: "VOLUME_UP",
+        desc: "Increase the volume."
+    },
+    {
+        value: "VOLUME_DOWN",
+        desc: "Decrease the volume."
+    },
+    {
+        value: "MUTE",
+        desc: "Mute a call."
+    },
+    {
+        value: "F_HOLD",
+        desc: "Place an active call on hold."
+    },
+    {
+        value: "F_CONFERENCE",
+        desc: "Press the Conf soft key."
+    },
+    {
+        value: "X",
+        desc: "Cancel actions or reject incoming calls or mute or un-mute calls."
+    },
+    {
+        value: "0",
+        desc: "Press the keypad 0."
+    },
+    {
+        value: "1",
+        desc: "Press the keypad 1."
+    },
+    {
+        value: "2",
+        desc: "Press the keypad 2."
+    },
+    {
+        value: "3",
+        desc: "Press the keypad 3."
+    },
+    {
+        value: "4",
+        desc: "Press the keypad 4."
+    },
+    {
+        value: "5",
+        desc: "Press the keypad 5."
+    },
+    {
+        value: "6",
+        desc: "Press the keypad 6."
+    },
+    {
+        value: "7",
+        desc: "Press the keypad 7."
+    },
+    {
+        value: "8",
+        desc: "Press the keypad 8."
+    },
+    {
+        value: "9",
+        desc: "Press the keypad 9."
+    },
+    {
+        value: "*",
+        desc: "Press the keypad *."
+    },
+    {
+        value: "POUND",
+        desc: "Press the keypad #."
+    },
+    {
+        value: "L1",
+        desc: "Press the line key L1"
+    },
+    {
+        value: "L2",
+        desc: "Press the line key L2"
+    },
+    {
+        value: "L3",
+        desc: "Press the line key L3"
+    },
+    {
+        value: "L4",
+        desc: "Press the line key L4"
+    },
+    {
+        value: "F1",
+        desc: "Press the soft key F1"
+    },
+    {
+        value: "F2",
+        desc: "Press the soft key F2"
+    },
+    {
+        value: "F3",
+        desc: "Press the soft key F3"
+    },
+    {
+        value: "F4",
+        desc: "Press the soft key F4"
+    },
+    {
+        value: "MSG",
+        desc: "Press the MESSAGE key."
+    },
+    {
+        value: "HEADSET",
+        desc: "Press the HEADSET key."
+    },
+    {
+        value: "RD",
+        desc: "Press the RD key."
+    },
+    {
+        value: "UP",
+        desc: "Press the up navigation key."
+    },
+    {
+        value: "DOWN",
+        desc: "Press the down navigation key."
+    },
+    {
+        value: "LEFT",
+        desc: "Press the left navigation key."
+    },
+    {
+        value: "RIGHT",
+        desc: "Press the right navigation key."
+    },
+    {
+        value: "Reboot",
+        desc: "Reboot the phone."
+    },
+    {
+        value: "AutoP",
+        desc: "Perform auto provisioning."
+    },
+    {
+        value: "DNDOn",
+        desc: "Activate the DND mode."
+    },
+    {
+        value: "DNDOff",
+        desc: "Deactivate the DND mode."
+    },
+    {
+        value: "OFFHOOK",
+        desc: "Pick up the handset."
+    },
+    {
+        value: "ONHOOK",
+        desc: "Hang up the handset."
+    },
+    {
+        value: "ANSWER",
+        desc: "Answer a call."
+    },
+    {
+        value: "Reset",
+        desc: "Reset a phone."
+    },
+    {
+        value: "CALLEND",
+        desc: "End a call."
+    }
+];
